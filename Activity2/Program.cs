55eb9494bacc2c414d86dbeae1ab5ec0437cbc04
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2022-10-19 16:59:03 +0700

    Creating method for get array elements

--- Activity2/Program.cs
+++ Activity2/Program.cs
@@ -25,6 +25,14 @@
             Console.WriteLine("\n===============================");
             Console.WriteLine("Enter Array Elmenents");
             Console.WriteLine("\n===============================");
+
+            //get array elements
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write("<"+(i+1)+">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
         }
     }
 }
